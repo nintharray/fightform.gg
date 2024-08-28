12114f5015a6769b0830ae0166a54c4842b31453
nintharray <167829823+nintharray@users.noreply.github.com>
2024-08-28 00:34:43 -0400

    feat: create placeholder landing page

---
+++ src/Placeholder.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const Placeholder: React.FC = () => {
+  return (
+		<img src="images/fightform-text-transparent.png" alt="FIGHTFORM logo" className="Placeholder"/>
+  );
+}
+
+export default Placeholder;
